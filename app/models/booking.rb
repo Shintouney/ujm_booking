b6c1461a03f33801738c38a9d25220f63f03b28a
Haitem ZEGHOUANI <nohiken42@gmail.com>
2019-03-13 10:43:34 +0100

    add rails i18n translations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,12 @@ class Booking < ApplicationRecord
   has_many :booking_products
   has_many :products, through: :booking_products
   belongs_to :student
+  validates :reservation_end_at, presence: true, allow_blank: false
+  validate :reservation_start_at_datetime
+
+  def reservation_start_at_datetime
+    errors.add(:base, I18n.t('booking.errors.reservation_start_at')) if (reservation_start_at < Date.today)
+  end
 
   aasm column: :status do
     state :pending, initial: true
