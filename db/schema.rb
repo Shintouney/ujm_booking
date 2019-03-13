cd690099db2a9e3631f5514b46c3845496b86f1c
Haitem ZEGHOUANI <nohiken42@gmail.com>
2019-03-13 16:54:20 +0100

    add aasm state to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_133159) do
+ActiveRecord::Schema.define(version: 2019_03_13_155211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_133159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.string "status"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
