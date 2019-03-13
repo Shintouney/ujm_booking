bc782f5eeeea18bdc13753ee33aae893b7777fe7
Haitem ZEGHOUANI <nohiken42@gmail.com>
2019-03-13 16:51:42 +0100

    add aasm state to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ApplicationRecord
+  include AASM
   belongs_to :category
   has_many :booking_products
   has_many :bookings, through: :booking_products
+
+  aasm column: :status do
+    state :available, initial: true
+    state :booked
+    state :archived
+
+    event :book do
+      transitions from: %i(available), to: %i(booked)
+    end
+
+    event :archive do
+      transitions from: %i(available booked), to: %i(archived)
+    end
+  end
 end
